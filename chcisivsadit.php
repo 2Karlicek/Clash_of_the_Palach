3e866654ebea47cf3b61a2d0ce3ee8fa67a8fe57
VDragoun <133709368+VDragoun@users.noreply.github.com>
2023-12-04 19:26:35 +0100

    Add files via upload

--- chcisivsadit.php
+++ chcisivsadit.php
@@ -5,12 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chci si vsadit</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        p {
+            margin-top: 300px;
+        }
+    </style>
 </head>
 <body>
-<?php
+    <?php
+    include "include.php";
+    ?>
 
-include "include.php";
-?>
     <p>Peníze na vsazení posílejte na účet: 670100-2221128998/6210</p>
     <p>O Vaše peníze bude dobře postaráno.</p>
 </body>
+</html>
